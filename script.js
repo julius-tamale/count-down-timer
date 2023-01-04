ff0bf733623adf8b231969edce264c4b108a69a6
julius-tamale <juliotamex@proton.me>
2023-01-04 22:32:47 +0200

    feat!: styles update for countdown timer

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 let countEl = document.querySelector('.count');
+let startBtn = document.querySelector('.green');
+let stopBtn = document.querySelector('.red');
+let resetBtn = document.querySelector('.reset')
